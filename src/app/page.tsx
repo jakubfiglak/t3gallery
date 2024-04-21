a9f5252e7c062d3f01afbddbbb7babcda3c8ee79
Jakub Figlak <jakub.figlak@gmail.com>
2024-04-21 13:56:48 +0200

    feat: basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+const mockUrls = [
+  "https://utfs.io/f/8cd03756-67a3-4428-aeb9-8f6b9433c9c2-4egksl.jpg",
+  "https://utfs.io/f/a41fbe84-b3ea-49e5-bdcb-c6293f914712-wg5ero.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
     <main>
-      <h1>Hello</h1>
-      <p>Gallery in progress</p>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <img
+            key={image.id}
+            src={image.url}
+            alt=""
+            className="w-48 object-cover"
+          />
+        ))}
+      </div>
     </main>
   );
 }
