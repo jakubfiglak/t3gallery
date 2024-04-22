e63add8dff86f0ce7453eb53ff0e3a4fda7d5335
Jakub Figlak <jakub.figlak@gmail.com>
2024-04-22 17:37:56 +0200

    feat: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/8cd03756-67a3-4428-aeb9-8f6b9433c9c2-4egksl.jpg",
   "https://utfs.io/f/a41fbe84-b3ea-49e5-bdcb-c6293f914712-wg5ero.jpg",
@@ -8,13 +10,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main>
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
           <img
-            key={image.id}
+            key={`${image.id}-${index}`}
             src={image.url}
             alt=""
             className="w-48 object-cover"
