23fb0c89ea8b4610029001307bb33e00ece3f90c
Jakub Figlak <jakub.figlak@gmail.com>
2024-04-22 17:41:09 +0200

    feat: force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/8cd03756-67a3-4428-aeb9-8f6b9433c9c2-4egksl.jpg",
   "https://utfs.io/f/a41fbe84-b3ea-49e5-bdcb-c6293f914712-wg5ero.jpg",
